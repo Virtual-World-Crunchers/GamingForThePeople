d2fb529e948c8107029f65442b9f76b00e0a52dd
Alison Wright <alwright_42@hotmail.com>
2021-09-30 23:02:29 +1000

    Revert "Added Unreal Project"
    This reverts commit a21116677b455d19790d99b9d59addb3979c6ace.

--- VWC-Project/Prototype1/Source/ShooterGame/Private/ShooterGameDelegates.h
+++
@@ -1,7 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-#pragma once
-
-/**
- * Called at startup to setup the FGameDelegates this game needs
- */
-void InitializeShooterGameDelegates();
--- VWC-Project/Prototype1/Source/ShooterGame/Private/Weapons/ShooterDamageType.cpp
+++
@@ -1,8 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "ShooterGame.h"
-#include "Weapons/ShooterDamageType.h"
-
-UShooterDamageType::UShooterDamageType(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer)
-{
-}
